6036113eeceabc513c70c4839f03c4c7c521eed8
Dost Muhammad Shah <dostmhd@gmail.com>
2015-01-11 11:57:29 +0400

    initial
    A code snippet in java that takes in a UNIX time stamp and returns time difference from current time

---
+++ time difference between now and a unix time stamp.java
@@ -0,0 +1,14 @@
+public static String caluculateTimeAgo(long timeStamp) {
+  long timeDiffernce;
+  long unixTime = System.currentTimeMillis() / 1000L;  //get current time in seconds. 
+  int j;
+  String[] periods = {"s", "m", "h", "d", "w", "m", "y", "d"};
+  // you may choose to write full time intervals like seconds, minutes, days and so on 
+  double[] lengths = {60, 60, 24, 7, 4.35, 12, 10};
+  timeDiffernce = unixTime - timeStamp;
+  String tense = "ago";
+  for (j = 0; timeDiffernce >= lengths[j] && j < lengths.length - 1; j++) {
+    timeDiffernce /= lengths[j];
+  }
+  return timeDiffernce + periods[j] + " " + tense;
+}
